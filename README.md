14fd47aa8da07503ee99acf5563f46268ffeb7c6
Ales Tsurko <ales.tsurko@gmail.com>
2021-03-17 16:24:53 +0300

    Configure CI

--- README.md
+++ README.md
@@ -1,14 +1,3 @@
 # Miller
 
 Modern front-end for Pure Data programming language.
-
-
-
-
-## Build
-
-This repo has submodules, initialize them with:
-
-```
-git submodule update --init
-```
---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+//! Miller is a modern front-end for Pure Data programming language.
+
+#![deny(
+    nonstandard_style,
+    rust_2018_idioms,
+    trivial_casts,
+    trivial_numeric_casts
+)]
+#![warn(
+    deprecated_in_future,
+    missing_docs,
+    unused_import_braces,
+    unused_labels,
+    unused_lifetimes,
+    unused_qualifications,
+    unreachable_pub
+)]
