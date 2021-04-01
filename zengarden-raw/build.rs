edcf02d64c15eb8df6e7115afa9c21d49939a178
Ales Tsurko <ales.tsurko@gmail.com>
2021-04-01 14:21:14 +0300

    Link libsndfile in build.rs

--- zengarden-raw/build.rs
+++ zengarden-raw/build.rs
@@ -24,7 +24,7 @@ fn compile() {
         .files(sources)
         .warnings(false)
         .flag("-lsndfile")
-        .flag("-regex")
+        .flag("-lregex")
         .flag("-pthread")
         .flag("-std=c++11");
 
